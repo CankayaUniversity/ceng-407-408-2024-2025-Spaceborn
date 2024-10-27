cd66eeea70daf5923095a4508a85c4d7f8a520a6
aslcerenhzr <ceren-hizir@hotmail.com>
2024-10-27 10:29:59 +0300

    p2

---
+++ Source/Spaceborn/SpacebornGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SpacebornGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASpacebornGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASpacebornGameMode();
+};
+
+
+
